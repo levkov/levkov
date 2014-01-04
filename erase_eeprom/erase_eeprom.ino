282a2484a7bd2fbad13b51abb4d24e79d574a931
levkov <levkov@levkov.tk>
2014-01-04 16:25:44 +0200

    Arduino Uno EEPROM Clear
    write a 0 to all 512 bytes of the EEPROM

---
+++ erase_eeprom/erase_eeprom.ino
@@ -0,0 +1,27 @@
+/*
+ * Arduino EEPROM Clear
+ *
+ * Sets all of the bytes of the EEPROM to 0.
+ * This example code is in the public domain.
+
+ */
+
+#include <EEPROM.h>
+
+void setup()
+{
+    pinMode(13, OUTPUT);     
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 512; i++)
+    EEPROM.write(i, 0);
+    
+  // turn the LED on when we're done
+    digitalWrite(13, HIGH);
+    delay(1000);   
+    digitalWrite(13, LOW);
+    
+}
+
+void loop()
+{
+}
